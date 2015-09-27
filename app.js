f4cfb19969d476e7d8221847eca7a0da2bb16468
Derevianko Liubov <dereviankoliubov@gmail.com>
2015-09-27 18:10:30 +0300

    Connect radio buttons and drop down menu (not good)

--- app.js
+++ app.js
@@ -5,9 +5,9 @@
 
 // 	app.controller('WorkController',function(){
 
-		
+
 // 	});
-  	
+
 // })();
 
 // angular
@@ -20,20 +20,22 @@
 //       address: 'Mountain View, CA'
 //     };
 //   });
-angular
-  .module('inputIconDemo', ['ngMaterial', 'ngMessages'])
-  .controller('DemoCtrl', function($scope) {
-    $scope.user = {
-      name: 'John Doe',
-      email: '',
-      phone: '',
-      address: 'Mountain View, CA'
-    };
+var app =angular.module('inputIconDemo', ['ngMaterial', 'ngMessages'])
+app.controller('DemoCtrl', function($scope) {
+	$scope.user = {
+		name: 'John Doe',
+		email: '',
+		phone: '',
+		address: 'Mountain View, CA'
+	};
 
-  });
-  inputIconDemo.controller('SelectOptGroupController',function($scope) {
-  	$scope.cars_marks=["Audi","BMW","Skoda","Volkswagen","KIA"];
+});
+app.controller('SelectOptGroupController',function($scope) {
+	$scope.cars=["Audi","BMW","Skoda"];
+	$scope.bikes=["Conandole","RIdeBike","Veloplaneta"];
+	$scope.snowboards=["GNU","Burton","Ride"];
 
 
-  });
 
+
+});
